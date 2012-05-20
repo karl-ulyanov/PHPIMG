cf2571fd63425875dc5ad08e2bbada99e14f7ac3
Nokaliejv Chakoski <nchakoski@hushmail.com>
2012-05-19 22:10:30 -0700

    configuration update.

--- config/boards.php
+++ config/boards.php
@@ -11,33 +11,39 @@
 $boardConfig = array(
     "anonymous" => array(
         "b" => array(
-            "dbname" => "random",
-            "username" => "dbuser",
-            "password" => "dbpass",
-            "host" => "localhost"
+            "db_name" => "random",
+            "db_user" => "dbuser",
+            "db_pass" => "dbpass",
+            "db_host" => "localhost"
             "www_ext_allowed" => "png,gif,jpg,jpeg",
             "www_thumb_dir" => "random",
             "www_max_filesize" => 59481,
+            "www_max_threads_per_page" => 10,
+            "www_max_threads" => 100
         ),  
         "gif" => array(
-            "dbname" => "gif",
-            "username" => "dbuser",
-            "password" => "dbpass",
-            "host" => "localhost",
+            "db_name" => "gif",
+            "db_user" => "dbuser",
+            "db_pass" => "dbpass",
+            "db_host" => "localhost",
             "www_ext_allowed" => "gif",
             "www_thumb_dir" => "gif",
             "www_max_filesize" => 59481,
+            "www_max_threads_per_page" => 10,
+            "www_max_threads" => 50
         )  
     ),
     "admin" => array(
         "admin" => array(
-            "dbname" => "administrators",
-            "username" => "dbuser",
-            "password" => "dbpass",
-            "host" => "localhost"
+            "db_name" => "administrators",
+            "db_user" => "dbuser",
+            "db_pass" => "dbpass",
+            "db_host" => "localhost"
             "www_ext_allowed" => "png,gif,jpg,jpeg",
             "www_thumb_dir" => "admin",
             "www_max_filesize" => 59481,
+            "www_max_threads" => 100,
+            "www_max_threads_per_page" => 20
         )
     )
 );  
